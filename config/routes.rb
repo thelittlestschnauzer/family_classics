05ca8bebe2ce1c753f637da13f7ecbad2df6fff0
Dana <doneil1580@gmail.com>
2018-11-20 13:39:57 -0500

    added bulma gem for styling with the meta tags for bulma and font-awesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+  
+  devise_scope :user do
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+  
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end 
+      
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
